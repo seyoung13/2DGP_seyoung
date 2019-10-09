174a26fcf26594ac50618608ec571fad45fd3375
seyoung <skview1201@gmail.com>
2019-10-09 16:31:50 +0900

    캐릭터 마우스 위치로 이동
    커밋 횟수: 2

--- drill 05/drill 05.py
+++ drill 05/drill 05.py
@@ -5,13 +5,16 @@ KPU_WIDTH, KPU_HEIGHT = 1280, 1024
 
 def handle_events():
     global running
-    global  x, y
+    global mouse_x, mouse_y
+    global char_x, char_y
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
         elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, KPU_HEIGHT - 1 - event.y
+            mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
+        elif event.type == SDL_MOUSEBUTTONDOWN:
+            char_x, char_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
@@ -22,15 +25,16 @@ character = load_image('animation_sheet.png')
 mycursor = load_image('hand_arrow.png')
 
 running = True
-x, y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
+char_x, char_y =200, 200
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, 200, 200)
-    mycursor.clip_draw(0, 0, 60, 60, x, y)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, char_x, char_y)
+    mycursor.clip_draw(0, 0, 60, 60, mouse_x+30, mouse_y-30)
     update_canvas()
     frame = (frame + 1) % 8
 
