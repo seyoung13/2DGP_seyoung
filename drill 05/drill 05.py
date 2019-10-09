6a58c35a79fddd7e007a64d2861e8f866bd58cbe
seyoung <skview1201@gmail.com>
2019-10-09 17:01:46 +0900

    error
    ?

--- drill 05/drill 05.py
+++ drill 05/drill 05.py
@@ -18,22 +18,34 @@ def handle_events():
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
+def character_move(p1, p2):
+    for i in range(0, 50+1, 2):
+        t=i/50
+        x = (1-t)*p1[0]+t*p2[0]
+        y = (1-t)*p1[1]+t*p2[1]
+        character.clip_draw(frame * 100, 100 * 1, 100, 100, p1, p2)
 
+#이미지 로드
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
 kpu_ground = load_image('KPU_GROUND.png')
 character = load_image('animation_sheet.png')
 mycursor = load_image('hand_arrow.png')
 
+#변수 초기화
 running = True
 mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
 char_x, char_y =200, 200
 frame = 0
+size=5
+points=[(char_x, char_y), (mouse_x, mouse_y)]
 hide_cursor()
 
+#실행
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, char_x, char_y)
+    
+    character_move(points[0],  points[1])
     mycursor.clip_draw(0, 0, 60, 60, mouse_x+30, mouse_y-30)
     update_canvas()
     frame = (frame + 1) % 8
