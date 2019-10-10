44c292fef5d86b5145122fcface5403378a306c0
seyoung <skivew1201@gmail.com>
2019-10-11 00:22:03 +0900

    플레이어 좌우 이동, 점프, 슈팅 구현

--- drill 05/drill 05.py
+++ drill 05/drill 05.py
@@ -1,6 +1,6 @@
 from pico2d import *
 
-KPU_WIDTH, KPU_HEIGHT = 1280, 1024
+KPU_WIDTH, KPU_HEIGHT = 1280, 762
 
 
 def handle_events():
@@ -14,16 +14,17 @@ def handle_events():
         elif event.type == SDL_MOUSEMOTION:
             mouse_x, mouse_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
-            char_x, char_y = event.x, KPU_HEIGHT - 1 - event.y
+            now_x, now_y = event.x, KPU_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
 
-def character_move(p1, p2):
-    for i in range(0, 50+1, 2):
-        t=i/50
-        x = (1-t)*p1[0]+t*p2[0]
-        y = (1-t)*p1[1]+t*p2[1]
-        character.clip_draw(frame * 100, 100 * 1, 100, 100, p1, p2)
+def character_move(x1, y1, x2, y2):
+    for i in range(0, 10+1):
+        t=i/10
+        x = (1-t)*x1+t*x2
+        y = (1-t)*y1+t*y2
+        character.clip_draw(frame * 100, 100 * face, 100, 100, x, y)
+
 
 #이미지 로드
 open_canvas(KPU_WIDTH, KPU_HEIGHT)
@@ -34,19 +35,24 @@ mycursor = load_image('hand_arrow.png')
 #변수 초기화
 running = True
 mouse_x, mouse_y = KPU_WIDTH // 2, KPU_HEIGHT // 2
-char_x, char_y =200, 200
+char_x, char_y=KPU_WIDTH // 2, KPU_HEIGHT // 2
+now_x, now_y =KPU_WIDTH // 2, KPU_HEIGHT // 2
 frame = 0
-size=5
-points=[(char_x, char_y), (mouse_x, mouse_y)]
+face=1
+upp=0
 hide_cursor()
 
 #실행
 while running:
     clear_canvas()
     kpu_ground.draw(KPU_WIDTH // 2, KPU_HEIGHT // 2)
-    
-    character_move(points[0],  points[1])
-    mycursor.clip_draw(0, 0, 60, 60, mouse_x+30, mouse_y-30)
+    if abs(mouse_x)-abs(char_x)>0:
+        face=1
+    else:
+        face=0
+    #character_move(char_x, char_y, now_x, now_y)
+    character.clip_draw(frame * 100, 100 * face, 100, 100, now_x, now_y)
+    mycursor.clip_draw(0, 0, 60, 60, mouse_x+30, mouse_y-30)     
     update_canvas()
     frame = (frame + 1) % 8
 
--- run/README.md
+++
@@ -1,2 +0,0 @@
-# 2DGP_seyoung
- 
